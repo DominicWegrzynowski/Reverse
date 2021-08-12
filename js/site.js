a5a6d416e22b651707c0353df5fb2582ea9d6fe6
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2021-08-12 16:09:00 -0400

    Finished reverse

--- js/site.js
+++ js/site.js
@@ -3,12 +3,19 @@ function getString() {
     let userInput = document.getElementById("userInput").value;
 
     if(typeof userInput === "string") {
-        let reversedString = userInput.split('').reverse().join('');
+        let reversedString = reverseString(userInput);
         displayString(reversedString);
     } 
     else {
         alert("Error: Please enter a string");
     }
+}
+
+function reverseString(string) {
+
+    const reversedString = string.split('').reverse().join('');
+    return reversedString;
+    
 }
 
 function displayString(reversedString) {
