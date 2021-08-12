c7af0101afa1be0a11c4d014e975be271c06fb53
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2021-08-12 15:24:58 -0400

    Completed App page

--- js/site.js
+++ js/site.js
@@ -1,5 +1,21 @@
+function getString() {
+    
+    let userInput = document.getElementById("userInput").value;
 
-//Call hello world
-function helloWorld() {
-    alert("hello world!");
+    if(typeof userInput === "string") {
+        let reversedString = userInput.split('').reverse().join('');
+        displayString(reversedString);
+    } 
+    else {
+        alert("Error: Please enter a string");
+    }
+}
+
+function displayString(reversedString) {
+
+    let templateRow = "";
+
+    templateRow += `<p>${reversedString}</p>`;
+    
+    document.getElementById("results").innerHTML = templateRow;
 }
